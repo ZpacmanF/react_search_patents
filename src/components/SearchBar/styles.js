487778ab8a2a20d9028b8ccdd533dab0c7dabb1d
ZpacmanF <joaocarvalho.2002@alunos.utfpr.edu.br>
2024-11-06 21:27:34 -0300

    feat: add styles searchBar

---
+++ src/components/SearchBar/styles.js
@@ -0,0 +1,13 @@
+export const searchBarStyles = {
+    wrapper: {
+      mb: 3,
+      width: '100%',
+    },
+    textField: {
+      '& .MuiOutlinedInput-root': {
+        '&:hover fieldset': {
+          borderColor: 'primary.main',
+        },
+      },
+    },
+  };
